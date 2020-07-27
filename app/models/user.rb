a66bf1fa2b5dc2f4c930ad21c8f1286c080eb385
mkopsho <michael.kopsho@gmail.com>
2020-07-27 15:23:34 -0400

    Add model and form validators for username, email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   has_many :tasks
   has_many :groups
   has_many :teams, through: :groups
+
   has_secure_password
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username
 end
