b6baa4b2aee8da0087bb485510cebf6ccab578d1
mkopsho <michael.kopsho@gmail.com>
2020-08-01 15:01:09 -0400

    Added assignee dropdowns on task creation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,8 +23,9 @@ class TasksController < ApplicationController
   end
 
   def create
+    binding.pry
     @task = Task.new(task_params)
-    @task.user_id = session[:user_id]
+    #@task.user_id = session[:user_id]
     if @task.save!
       redirect_to list_path(@task.list_id)
     else
